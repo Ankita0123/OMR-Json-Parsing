44cc775e25be87fa1b0f59972e8d3182cc168ba0
Ankita0123 <Ankita0123@users.noreply.github.com>
2014-07-16 08:02:47 +0530

    Create PhoneBookBean.java

---
+++ PhoneBookBean.java
@@ -0,0 +1,27 @@
+package com.beans;
+
+public class PhonebookBean {
+	String fname;
+	String lname;
+	String ph_num;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getPh_num() {
+		return ph_num;
+	}
+	public void setPh_num(String ph_num) {
+		this.ph_num = ph_num;
+	}
+	
+
+}
